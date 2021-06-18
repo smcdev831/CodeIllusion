c50e6ec7071800758a3c7a14ed15e93d181259e4
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-06-17 19:02:12 -0500

    Create Lanterns.js

---
+++ Lanterns.js
@@ -0,0 +1,35 @@
+var castleImage;
+var lanternImage;
+var lanternPositionX = [];
+var lanternPositionY = [];
+var lanternVelocityY = [];
+var lanternCount = 16;
+
+function preload() {
+  castleImage = loadImage('castle.png');
+  lanternImage = loadImage('lantern.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for (var i = 0; i < lanternCount; i++) {
+    lanternPositionX[i] = random(width);
+    lanternPositionY[i] = random(height);
+    lanternVelocityY[i] = random(0.2, 0.7);
+  }
+}
+
+function draw() {
+  imageMode(CENTER);
+  image(castleImage, width/2, height/2, width, height);
+
+  for (var i = 0; i < lanternCount; i++) {
+    image(lanternImage, lanternPositionX[i], lanternPositionY[i], 80, 80);
+    lanternPositionY[i] -= lanternVelocityY[i];
+    if (lanternPositionY[i] < -40) {
+      lanternPositionX[i] = random(width);
+      lanternPositionY[i] = height + 40;
+    }
+  }
+}
